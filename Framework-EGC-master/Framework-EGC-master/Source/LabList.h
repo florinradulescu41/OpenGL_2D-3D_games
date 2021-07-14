1863ca1536333ec9bd31afd29f91d2f977c0e85a
florinradulescu41 <75788365+florinradulescu41@users.noreply.github.com>
2021-07-14 12:26:32 +0300

    Update LabList.h

--- Framework-EGC-master/Framework-EGC-master/Source/LabList.h
+++ Framework-EGC-master/Framework-EGC-master/Source/LabList.h
@@ -1,5 +1,14 @@
 #pragma once
 
-#include <Laboratoare/Tema1/Tema1.h>
-#include <Laboratoare/Tema2/Tema2.h>
+#include <Labs/Lab1.h>
+#include <Labs/Lab2.h>
+#include <Labs/Lab3.h>
+#include <Labs/Lab4.h>
+#include <Labs/Lab5.h>
+#include <Labs/Lab6.h>
+#include <Labs/Lab7.h>
+#include <Labs/Lab8.h>
+#include <Labs/Lab9.h>
+#include <Archer_OpenGL/Tema1.h>
+#include <Skyroads_OpenGL/Tema2.h>
 
